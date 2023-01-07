185971a46615cc1306824089adfdebf25efea278
Pedro da Conceicao <pedrocardo007@hotmail.com>
2023-01-06 21:11:00 -0300

    Parada no dia 6 de janeiro 44:05

--- jest.config.ts
+++ jest.config.ts
@@ -1,20 +1,26 @@
+import { Config } from 'jest';
 import { pathsToModuleNameMapper} from 'ts-jest'
-import {} from './tsconfig.json'
+import { compilerOptions } from './tsconfig.json';
 
-export default {
+const config: Config = {
     "moduleFileExtensions": [
       "js",
       "json",
       "ts"
     ],
-    "rootDir": "src",
     "testRegex": ".*\\.spec\\.ts$",
     "transform": {
       "^.+\\.(t|j)s$": "ts-jest"
     },
+    
     "collectCoverageFrom": [
       "**/*.(t|j)s"
     ],
     "coverageDirectory": "../coverage",
-    "testEnvironment": "node"
+    "testEnvironment": "node",
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+      prefix: '<rootDir>/',
+    }),
 };
+
+export default config;
--- src/application/use-cases/cancel-notification-spec.ts
+++ src/application/use-cases/cancel-notification-spec.ts
@@ -2,10 +2,11 @@ import { InMemoryNotificationsRepository } from "../../../test/repositories/in-m
 import { CancelNotification } from "./cancel-notification";
 import { Notification } from "@application/entities/notification";
 import { Content } from "@application/entities/content";
+import { NotificationNotFound } from "./errors/notification-not-found";
 
 
 describe('Cancel notification', () => {
-    it('should be able to send a notification', async () => {
+    it('should be able to cancel a notification', async () => {
         const notificationsRepository = new InMemoryNotificationsRepository();
         const cancelNotification = new CancelNotification(notificationsRepository);
 
@@ -23,7 +24,18 @@ describe('Cancel notification', () => {
         });
 
         expect(notificationsRepository.notifications[0].canceledAt).toEqual(
-            expect.any(data)
+            expect.any(Date),
         );
+    });
+
+    it('should not be able to cancel a non existing notification', async () => {
+        const notificationsRepository = new InMemoryNotificationsRepository();
+        const cancelNotification = new CancelNotification(notificationsRepository);
+
+        expect(() => {
+            return cancelNotification.execute({
+                notificationId: 'fake-notification-id',
+            });
+        }).rejects.toThrow(NotificationNotFound);
     });
 });
